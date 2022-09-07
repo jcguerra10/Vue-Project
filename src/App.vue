1835df696a299b399593e8efc224855e698d9c7f
jcguerra10 <juancamiloguerratabares@gmail.com>
2022-09-07 08:23:30 -0500

    localStorage

--- src/App.vue
+++ src/App.vue
@@ -19,57 +19,29 @@ import NewPublis from "./views/NewPublis.vue";
 export default {
     data() {
         return {
-            publisArray: [
-                {
-                    id: "1",
-                    img: "../resources/img1.jpeg",
-                    description: "Lorem ipsum dolor sit amet, consectetur adip",
-                    game: "EjMost",
-                    label: "Picture",
-                    genre: "Thriller",
-                },
-                {
-                    id: "2",
-                    img: "",
-                    description: "Dolor sit amet, lorem ipsum consectetur adip",
-                    game: "EjWars",
-                    label: "Picture",
-                    genre: "Suspenso",
-                },
-                {
-                    id: "3",
-                    img: "",
-                    description: "Morbi vitae, consectetur adip",
-                    game: "Ejcasse",
-                    label: "Post",
-                    genre: "Comedy",
-                },
-                {
-                    id: "4",
-                    img: "",
-                    description: "Morbi, Vitae consectetur adip",
-                    game: "Ejcasse",
-                    label: "Post",
-                    genre: "Horror",
-                },
-            ],
+            publisArray: [],
         };
+    },
+    mounted() {
+        if (localStorage.getItem("publis")) {
+            this.publisArray = JSON.parse(localStorage.getItem('publis'));
+        }
     },
     methods: {
         handleClick() {
             this.$router.push({
-                name: "publis", //use name for router push
+                name: "publis", 
                 params: this.publisArray,
             });
         },
         addPubli(publi) {
             console.log("publi");
             this.publisArray = [...this.publisArray, publi];
+            const parsed = JSON.stringify(this.publisArray);
+            localStorage.setItem("publis", parsed);
         },
-        handleSendMessage(event) {
-            // Our event handler gets the event, as well as any
-            // arguments the child passes to the event
-            console.log('guardee', event);
+        persist() {
+            localStorage.publisArray = JSON.stringify(this.publisArray);
         },
     },
     components: {
